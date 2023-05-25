65ebba8d2cc7e290f0c7aea71b43207440e467aa
Alex Kirszenberg <alex.kirszenberg@vercel.com>
2023-05-25 18:31:22 +0200

    Lint and fmt

--- packages/next-swc/crates/next-core/src/next_server/mod.rs
+++ packages/next-swc/crates/next-core/src/next_server/mod.rs
@@ -2,5 +2,7 @@ pub(crate) mod context;
 pub(crate) mod resolve;
 pub(crate) mod transforms;
 
-pub use context::{get_server_compile_time_info, get_server_module_options_context,
-    get_server_resolve_options_context, ServerContextType,};
+pub use context::{
+    get_server_compile_time_info, get_server_module_options_context,
+    get_server_resolve_options_context, ServerContextType,
+};
